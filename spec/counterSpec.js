a713675c7c1f1200b20ea4e44321246fdeb0c9e6
Merve Silk <merve.silk@gmail.com>
2015-06-29 22:17:40 +0100

    Refactors code and adds two more tests which include return messages

--- spec/counterSpec.js
+++ spec/counterSpec.js
@@ -1,23 +1,37 @@
 describe("Counter", function() {
   var counter;
 
+    beforeEach(function() {
+      counter = new Counter();
+    });
+
   describe("by default, ", function() {
     it("starts at Life Level: '20'", function() {
-      var counter = new Counter();
       expect(counter.lifeLevel).toEqual(20);
     });
 
     it("has a Life Level Limit of '999'", function() {
-      var counter = new Counter();
       counter.addLife(980);
       expect(counter.lifeLevel).toEqual(999);
     });
 
-    it("has a Life Level Minimum of '0'", function() {
-      var counter = new Counter();
+    it("cannot allow the Life Level to go below '0'", function() {
       counter.decreaseLife(21);
       expect(counter.lifeLevel).toEqual(0);
     });
   });
+
+  describe("will return the following Message if", function() {
+    it("Player's Life Level reaches below '1'", function() {
+      expect(counter.decreaseLife(20)).toEqual("YOU ARE DEAD, YOUR OPPONENT WILL FEAST FROM THE MARROW IN YOUR BONES AND DANCE UPON YOUR GRAVES");
+    });
+
+    it("Player's Life Level tries to go over '999'", function() {
+      expect(counter.addLife(979)).toEqual("NICE TRY....NO ONE LIVES THAT LONG IN MAGIC...CONSIDER YOURSELF THRICE CURSED FOR BEHAVIOUR UNBECOMING!!!!");
+    });
+
+
+  });
+
 
 });
--- src/counter.js
+++ src/counter.js
@@ -3,8 +3,9 @@ var Counter = function() {
 };
 
 Counter.prototype.addLife = function(lifeTokens) {
-  if (this.lifeLevel + lifeTokens >= 1000) {
+  if (this.lifeLevel + lifeTokens >= 999) {
     this.lifeLevel = 999;
+    return "NICE TRY....NO ONE LIVES THAT LONG IN MAGIC...CONSIDER YOURSELF THRICE CURSED FOR BEHAVIOUR UNBECOMING!!!!";
   } else {
     this.lifeLevel += lifeTokens;
   }
@@ -13,6 +14,7 @@ Counter.prototype.addLife = function(lifeTokens) {
 Counter.prototype.decreaseLife = function(lifeTokens) {
   if (this.lifeLevel - lifeTokens <= 0) {
     this.lifeLevel = 0;
+    return "YOU ARE DEAD, YOUR OPPONENT WILL FEAST FROM THE MARROW IN YOUR BONES AND DANCE UPON YOUR GRAVES";
   } else {
     this.lifeLevel -= lifeTokens;
   }
