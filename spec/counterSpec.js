ebc4047d2ecdb8e5dbbc5f66aaede0d0778d98cb
Merve Silk <merve.silk@gmail.com>
2015-06-29 16:58:37 +0100

    Adds second passing test, counter increases and has limit of 999

--- spec/counterSpec.js
+++ spec/counterSpec.js
@@ -1,10 +1,16 @@
 describe("Counter", function() {
   var counter;
 
-  describe("by default, always", function() {
+  describe("by default, ", function() {
     it("starts at Life Level: '20'", function() {
       var counter = new Counter();
       expect(counter.lifeLevel).toEqual(20);
+    });
+
+    it("has a Life Level Limit of '999'", function() {
+      var counter = new Counter();
+      counter.addLife(980);
+      expect(counter.lifeLevel).toEqual(999);
     });
 
   });
--- src/counter.js
+++ src/counter.js
@@ -1,3 +1,11 @@
 var Counter = function() {
   this.lifeLevel = 20;
 };
+
+Counter.prototype.addLife = function(lifeTokens) {
+  if (this.lifeLevel + lifeTokens >= 1000) {
+    this.lifeLevel = 999;
+  } else {
+    this.lifeLevel += lifeTokens;
+  }
+};
