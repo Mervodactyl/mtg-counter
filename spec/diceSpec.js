397b29e6ff01d0787d560c88a0369e60b113aaaf
Merve Silk <merve.silk@gmail.com>
2015-06-30 19:44:45 +0100

    Refactors unit tests, and adds new increaseDice test

--- spec/diceSpec.js
+++ spec/diceSpec.js
@@ -1,27 +1,44 @@
 describe("Dice", function() {
   var dice;
 
+    beforeEach(function() {
+      dice = new Dice();
+    });
+
   describe("should ", function() {
-    it("start at '0'", function() {
-      var dice = new Dice();
-      expect(dice.dieFace).toEqual(0);
-    });
-
-    it("be able to start at another Face setting, if desired", function() {
-      var dice = new Dice();
-      dice.setFace(15);
-      expect(dice.dieFace).toEqual(15);
-    });
-
-    it("be limited to '20' sides", function() {
-      var dice = new Dice();
-      dice.setFace(21);
+    it("start at '20'", function() {
       expect(dice.dieFace).toEqual(20);
     });
 
-    // it("not be able to go below ''", function() {
-    //
-    // });
+    it("not be able to go Below '1'", function() {
+      dice.decreaseDice(20);
+      expect(dice.dieFace).toEqual(1);
+    });
+
+    it("be Limited to a maximum of '20' sides", function() {
+      dice.increaseDice(1);
+      expect(dice.dieFace).toEqual(20);
+    });
+  });
+
+  describe("can change its Original Starting point to begin at a ", function() {
+    it("Lower face setting", function() {
+      dice.decreaseDice(5);
+      expect(dice.dieFace).toEqual(15);
+    });
+
+    it("or a Higher face setting", function() {
+      dice.decreaseDice(10);
+      dice.increaseDice(6);
+      expect(dice.dieFace).toEqual(16);
+    });
+  });
+
+  describe("Main Purpose is to ", function() {
+    xit("generate a random Number from the Player's throw", function() {
+      dice.rollDice();
+      expect();
+    });
   });
 
 });
--- src/dice.js
+++ src/dice.js
@@ -1,11 +1,19 @@
 var Dice = function() {
-  this.dieFace = 0;
+  this.dieFace = 20;
 };
 
-Dice.prototype.setFace = function(faceNumber) {
-  if (this.dieFace + faceNumber >= 20) {
+Dice.prototype.decreaseDice = function(lowerDieNumber) {
+  if (this.dieFace - lowerDieNumber <= 1) {
+    this.dieFace = 1;
+  } else {
+    this.dieFace -= lowerDieNumber;
+  }
+};
+
+Dice.prototype.increaseDice = function(higherDieNumber) {
+  if (this.dieFace + higherDieNumber >= 20) {
     this.dieFace = 20;
   } else {
-    this.dieFace += faceNumber;
+    this.dieFace += higherDieNumber;
   }
 };
