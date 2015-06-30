45609d9ee1dce5e91da6235d9e577270d8932c04
Merve Silk <merve.silk@gmail.com>
2015-06-30 23:38:48 +0100

    Three tests passing, adds limitations

--- spec/poisonSpec.js
+++ spec/poisonSpec.js
@@ -1,11 +1,25 @@
 describe("Poison", function() {
   var poison;
 
-  describe("beigns ", function() {
-    it("at '0'", function() {
+  describe("beigns with ", function() {
+    it("'0' Poison counters", function() {
       var poison = new Poison();
       expect(poison.toxicityLevel).toEqual(0);
     });
 
+    describe("is Limited to ", function() {
+      it("a Maximum of '10' Poison counters", function() {
+        var poison = new Poison();
+        poison.increaseToxicity(11);
+        expect(poison.toxicityLevel).toEqual(10);
+      });
+
+      it("a Minimum of '0' Poison counters", function() {
+        var poison = new Poison();
+        poison.decreaseToxicity(1);
+        expect(poison.toxicityLevel).toEqual(0);
+      });
+    });
+    
   });
 });
--- src/poison.js
+++ src/poison.js
@@ -1,3 +1,19 @@
 var Poison = function() {
   this.toxicityLevel = 0;
 };
+
+Poison.prototype.increaseToxicity = function(poisonCounters) {
+  if (this.toxicityLevel + poisonCounters >= 10) {
+    this.toxicityLevel = 10;
+  } else {
+    this.toxicityLevel += poisonCounters;
+  }
+};
+
+Poison.prototype.decreaseToxicity = function(poisonCounters) {
+  if (this.toxicityLevel - poisonCounters <= 0) {
+    this.toxicityLevel = 0;
+  } else {
+    this.toxicityLevel -= poisonCounters;
+  }
+};
